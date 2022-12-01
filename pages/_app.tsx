8295b561cd27fdfa1747b797f630a0db352261a3
Matthew Browning <mjbrowningg@gmail.com>
2022-12-01 12:31:11 -0800

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Head from "next/head";
+import {MantineProvider} from "@mantine/core";
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient()
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>NAMR</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+            </Head>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                <QueryClientProvider client={queryClient}>
+                    <Component {...pageProps} />
+                </QueryClientProvider>
+            </MantineProvider>
+        </>
+    )
 }
